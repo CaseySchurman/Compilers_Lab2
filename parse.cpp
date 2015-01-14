5ea74b98af9cc16dc3d0dc0a2db01461496d9e0e
Casey Schurman <casey.schurman@oit.edu>
2015-01-14 01:24:46 +0000

    Lab 2 Finish parse.cpp

--- parse.cpp
+++ parse.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "lex.h"
 #include "parse.h"
+#include "tokens.h"
 
 int yylval = 0;
 int token = -1;
@@ -30,11 +31,248 @@ void UnGetToken()
 {
     token = oldToken;
 }
-//*******************************************
+//******************************************
 bool FindPROG()
 {
-    Error("Program");
-    return false;
+    if (!FindSTMTS()) 
+    {
+        return false;
+    }
+    
+    int token = GetToken();
+    
+    if(token != END)
+    {
+        Error("end");
+        return false;
+    }
+    
+    return true;
+}
+//******************************************
+bool FindSTMTS()
+{
+    if (FindSTMT())
+    {
+        if(!FindSMTS())
+        {
+            Error("statements");
+            return false;
+        }
+        
+        return true;
+    }
+    
+    return true;
+}
+//******************************************
+bool FindSTMT()
+{
+    if (!FindEXPR())
+    {
+        Error("expression");
+        return false;
+    }
+    
+    int token = GetToken();
+    
+    if(token != ';')
+    {
+        Error(";");
+        return false;
+    }
+    
+    return true;
+}
+//******************************************
+bool FindEXPR()
+{
+    int token = GetToken();
+    
+    if(token != '(')
+    {
+        UnGetToken();
+        
+        if(!FindTERM())
+        {
+            Error("term");
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    else
+    {
+        if(!FindEXPR())
+        {
+            Error("expression");
+            return false;
+        }
+        
+        token = GetToken();
+        
+        if(token != ')')
+        {
+            Error(")");
+            return false;
+        }
+        
+        if(!FindEXPR_P())
+        {
+            Error("expression_prime");
+            return false;
+        }
+        
+        return true;
+    }
+}
+//******************************************
+bool FindEXPR_P()
+{
+    if(FindPLUSOP())
+    {
+        int token = GetToken();
+        
+        if(token != '(')
+        {
+            Error("(");
+            return false;
+        }
+        
+        if(!FindEXPR())
+        {
+            Error("expression");
+            return false;
+        }
+        
+        token = GetToken();
+        
+        if(token != ')')
+        {
+            Error(")");
+        }
+        
+        if(!FindEXPR_P())
+        {
+            Error("expression_prime");
+            return false;
+        }
+        
+        return true;
+    }
+}
+//******************************************
+bool FindPLUSOP()
+{
+    int token = GetToken();
+    
+    if(token != '+')
+    {
+        if(token != '-')
+        {
+            Error("plusop");
+            return false;
+        }
+        
+        return true;
+    }
+    
+    return true;
+}
+//******************************************
+bool FindTERM()
+{
+    int token = GetToken();
+    
+    if(token != '[')
+    {
+        if(token != INT_VAL)
+        {
+            Error("term");
+            return false;
+        }
+        
+        return true;
+    }
+    
+    if(!FindEXPR())
+    {
+        Error("expression");
+        return false;
+    }
+    
+    token = GetToken();
+    
+    if(token != ']')
+    {
+        Error("]");
+        return false;
+    }
+    
+    if(!FindTERM_P())
+    {
+        Error("term_prime");
+        return false;
+    }
+    
+    return true;
+}
+//******************************************
+bool FindTERM_P()
+{
+    if(FindTIMESOP())
+    {
+        int token = GetToken();
+        
+        if(token != '[')
+        {
+            Error("[");
+            return false;
+        }
+        
+        if(!FindEXPR())
+        {
+            Error("expression");
+            return false;
+        }
+        
+        token = GetToken();
+        
+        if(token != ']')
+        {
+            Error("]");
+            return false;
+        }
+        
+        if(!FindTERM_P())
+        {
+            Error("term_prime");
+            return false;
+        }
+        
+        return true;
+    }
+    
+    return true;
+}
+//******************************************
+bool FindTIMESOP()
+{
+    int token = GetToken();
+    
+    if(token != '*')
+    {
+        if(token != '/')
+        {
+            Error("times_op");
+            return false;
+        }
+        return true;
+    }
+    
+    return true;
 }
 /*******************************************
 bool FindExample()
@@ -53,4 +291,3 @@ bool FindExample()
     return true;
 }
 */
-
